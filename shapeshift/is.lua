db8b5882affa54990300093b435cf682771a24a4
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2020-12-04 12:54:41 +0100

    Fix is functions not returning the value

--- shapeshift/is.lua
+++ shapeshift/is.lua
@@ -3,7 +3,7 @@ local __is = {}
 function __is:__index(key)
 	self[key] = function(value)
 		if type(value) == tostring(key) then
-			return true
+			return value
 		else
 			return nil, key .. " expected, got " .. type(value)
 		end
